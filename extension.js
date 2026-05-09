b04122f0c22481baff7db3446df07cc8644326d5
Tomé Carvalho <tomecarvalho01@gmail.com>
2026-05-09 21:44:35 +0100

    Editor/tool config

--- extension.js
+++ extension.js
@@ -15,12 +15,10 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import {Extension} from "resource:///org/gnome/shell/extensions/extension.js";
 
-export default class PlainExampleExtension extends Extension {
-    enable() {
-    }
+export default class CenterMouseShortcut extends Extension {
+    enable() {}
 
-    disable() {
-    }
+    disable() {}
 }
